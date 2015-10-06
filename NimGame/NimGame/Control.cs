0f10aa582b6f75aff814918df974a8ffb7f090f6
enochfin <enochfinley@gmail.com>
2015-10-05 21:08:06 -0600

    Added Control class, getPossibleMoves()

---
+++ NimGame/NimGame/Control.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame
+{
+    class Control
+    {
+        public static List<Board> getPossibleMoves(Board start, Dictionary<Board, HashOfBoards.meanOcc> boards)
+        {
+            //a = d b = e c > f
+            //a = d b > e c = f
+            //a > d b = e c = f
+            List<Board> retVal = new List<Board>();
+            foreach (KeyValuePair<Board, HashOfBoards.meanOcc> element in boards)
+            {
+                if (start.isValid(element.Key)){
+                    retVal.Add(element.Key);
+                }
+            }
+
+            return retVal;
+        }
+    }
+}
